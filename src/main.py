f223c99b23d47b06eae3026ebd8379c5eec2ddd2
luigi-bassorici <113697812+luigi-bassorici@users.noreply.github.com>
2022-09-18 14:18:20 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print('oi')
+nome = input('digite seu nome: ')
+idade = int(input('digite sua idade: '))
+peso = float(input('digite seu peso: '))
+
+print(f'''
+Olá {nome}:
+sua idade é : {idade}
+seu peso é : {peso}
+''')
